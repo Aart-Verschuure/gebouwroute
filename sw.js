15ece122af6e61c999c51a509586e0e80171ddc9
Aart-Verschuure <51282@hoornbeeck.nl>
2026-09-09 10:52:17 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'gebouwenroute-app-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './game.js',
+  './manifest.json',
+  'https://cdn-icons-png.flaticon.com/512/566/566312.png' // Zorg dat je icoontje hier ook staat
+];
+// ... de rest van je Service Worker code ...
+
+// Installatie: bestanden opslaan in cache
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Activering: oude caches verwijderen
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetch: serveer bestanden uit cache als we offline zijn
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
